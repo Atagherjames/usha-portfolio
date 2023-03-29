08198b289e31879f38591deeb58ce06477bc2fc8
Atagher James <113046523+Atagherjames@users.noreply.github.com>
2023-03-29 12:37:14 +0000

    update

--- script.js
+++ script.js
@@ -1,68 +1,6 @@
-const body = document.querySelector('body')
-const light = document.querySelector('.bi-brightness-low-fill')
-const dark = document.querySelector('.bi-moon-fill')
-
-dark.addEventListener('click', () => {
-  if (document.body.getAttribute('data-theme') === 'light') {
-    document.body.setAttribute('data-theme', 'dark')
-    localStorage.setItem('data-theme', 'dark')
-  }
-  else {
-    document.body.setAttribute('data-theme', 'dark')
-  }
-})
-
-light.addEventListener('click', () => {
-  if (document.body.getAttribute('data-theme') === 'dark') {
-    document.body.setAttribute('data-theme', 'light')
-    localStorage.setItem('data-theme', 'light')
-  }
-  else {
-    document.body.setAttribute('data-theme', 'light')
-  }
-})
-
-let theme = localStorage.getItem('data-theme')
-console.log(theme)
-if (theme) {
-  document.body.setAttribute('data-theme', theme)
-}
-
-
-// hamburger menu
-const open = document.querySelector('.open')
-const close = document.querySelector('.close')
-const menu = document.querySelector('.menu')
-const header = document.querySelector('.header')
-
-const custom = () => {
-  open.style.display = 'block'
-  close.style.display = 'none'
-  header.style.height = '90px'
-  menu.style.display = 'none'
-}
-
-menu.childNodes.forEach((child) => {
-  child.addEventListener('click', () => {
-    if (window.innerWidth <= 650) {
-      custom()
-    }
-  })
-})
-
-open.addEventListener('click', () => {
-  close.style.display = 'block'
-  open.style.display = 'none'
-  header.style.height = '220px'
-  menu.style.display = 'block'
-})
-
-close.addEventListener('click', () => {
-  custom()
-})
-
 // About me
-const tab = document.querySelectorAll('#tab')
+
+const tab = document.querySelectorAll('.tablink')
 const tabcontent = document.querySelectorAll('.tabcontent')
 
 
@@ -82,14 +20,13 @@ tab.forEach((item) => {
     else {
       tabcontent[0].style.display = 'block'
     }
-    item.classList.add('tab');
+    item.classList.add('active');
   })
 })
 
 function remove() {
   tab.forEach((item) => {
-    item.classList.remove('tab')
-
+    item.classList.remove('active')
   })
 }
 
